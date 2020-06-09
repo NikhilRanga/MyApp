82d00947f69b19eb9dc4faf5931b64e9e997d113
Pavan Kalyan <pavankalyaninugurthi@gmail.com>
2020-06-09 14:39:54 +0530

    Initial Commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+Y
+)app/src/main/res/layout/activity_main.xml,7\6\76b07e40f24cce093536731477540f12e7da3cb6
+e
+5app/src/main/java/com/example/myapp/MainActivity.java,e\7\e773a4409b1f4ee8216b8db716b4abcf6b42e482
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+f
+6app/src/main/java/com/example/myapp/MainActivity2.java,d\2\d29e159f0d5bfa5ba922219b482b1a8f3fece3aa
---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    EditText uname,pass;
+    private RequestQueue queue;
+    JsonObjectRequest objectRequest;
+    JSONObject data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.button);
+        uname = (EditText) findViewById(R.id.uname);
+        pass = (EditText) findViewById(R.id.pass);
+
+        String URL = "http://ec2-3-7-131-60.ap-south-1.compute.amazonaws.com/login";
+        data = new JSONObject();
+        try {
+            data.put("username","admin");
+            data.put("password","admin@123");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        queue = Volley.newRequestQueue(this);
+        objectRequest = new JsonObjectRequest(Request.Method.POST,
+                URL,
+                data,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String accessTkn = response.getString("access_token");
+                            MainActivity2.text.setText(accessTkn);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast toast = Toast.makeText(getApplicationContext(),"Not a valid user",Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                queue.add(objectRequest);
+                openActivity2();
+            }
+        });
+    }
+
+    private void openActivity2() {
+        Intent intent = new Intent(this,MainActivity2.class);
+        startActivity(intent);
+    }
+}
