4ae81744a8a995b0b45bf9efb78b6eefddc21bf0
namyareddy <gnamyareddy25@gmail.com>
2020-06-26 14:09:29 +0530

    Updated

--- app/src/main/java/com/example/myapp/homepage.java
+++ app/src/main/java/com/example/myapp/homepage.java
@@ -1,11 +1,26 @@
 package com.example.myapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class homepage extends AppCompatActivity {
     private Button button1;
@@ -13,42 +28,39 @@ public class homepage extends AppCompatActivity {
     private TextView tev2;
     static String accessTkn;
     static String eid;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
         getSupportActionBar().setTitle("Homepage");
-
         button1 = (Button) findViewById(R.id.button);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i1 = new Intent(homepage.this,MainActivity.class);
+                Intent i1 = new Intent(homepage.this, MainActivity.class);
                 startActivity(i1);
             }
         });
-        tev1= (TextView) findViewById(R.id.textView1);
+        tev1 = (TextView) findViewById(R.id.textView1);
         tev1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rolelist.accessTkn =accessTkn;
-                rolelist.eid=eid;
-                Intent i2 = new Intent(homepage.this,rolelist.class);
+                rolelist.accessTkn = accessTkn;
+                rolelist.eid = eid;
+                Intent i2 = new Intent(homepage.this, rolelist.class);
                 startActivity(i2);
 
             }
         });
-        tev2= (TextView) findViewById(R.id.textView2);
+        tev2 = (TextView) findViewById(R.id.textView2);
         tev2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                status.accessTkn =accessTkn;
-                status.eid=eid;
-                Intent i3 = new Intent(homepage.this,status.class);
+                status.accessTkn = accessTkn;
+                status.eid = eid;
+                Intent i3 = new Intent(homepage.this, status.class);
                 startActivity(i3);
             }
         });
-
     }
 }
